978b489ea8b2663f00ffa336a4d4bfa17056c76a
MAKAVELI <neogood@yahoo.com>
2018-03-26 09:38:30 +0430

    users working , except security

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -6,6 +6,7 @@
         <thead>
            <tr>
              <th>ID</th>
+             <th>Photo</th>
              <th>Name</th>
              <th>Email</th>
              <th>Role</th>
@@ -19,9 +20,10 @@
              @foreach($users as $user)
           <tr>
              <td>{{$user->id}}</td>
-             <td>{{$user->name}}</td>
+             <td><img height="50px" class="" src="{{$user->photo ? $user->photo->file : 'http://placehold.it/50x50'}}" alt=""></td>
+             <td><a href="{{route('admin.users.edit', $user->id)}}">{{$user->name}}</a></td>
              <td>{{$user->email}}</td>
-             <td>{{$user->role->name}}</td>
+             <td>{{$user->role ? $user->role->name : "user has no role"}}</td>
              <td>{{$user->is_active = 1 ? 'Active': 'Not active'}}</td>
              <td>{{$user->created_at->diffForHumans()}}</td>
              <td>{{$user->updated_at->diffForHumans()}}</td>
